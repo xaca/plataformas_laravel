f3f234b0816261a6726685f5962611f096370426
xaca <and1res@hotmail.com>
2022-04-25 13:28:51 -0500

    primera parte proyecto

---
+++ resources/views/menu.blade.php
@@ -0,0 +1,28 @@
+<ul class="menu">
+    <li>
+    <a href="javascript:void(0);" id="item_1">Home</a>
+    </li>
+    <li class="active">
+    <a href="javascript:void(0);" id="item_2">About Us</a>
+    </li>
+    <li>
+    <!-- First Tier Drop Down -->
+    <label for="drop-2" class="toggle">
+    Dropdown <span class="fa fa-angle-down" aria-hidden="true"></span>
+    </label>
+    <a href="#">Dropdown 
+    <span class="fa fa-angle-down" aria-hidden="true"></span>
+    </a>
+    <input type="checkbox" id="drop-2" />
+    <ul>
+        <li><a href="javascript:void(0);" id="item_3" class="drop-text">Services</a></li>
+        <li><a href="javascript:void(0);" id="item_4" class="drop-text">Single Page</a></li>
+    </ul>
+    </li>
+    <li>
+    <a href="javascript:void(0);" id="item_5">Collections</a>
+    </li>
+    <li>
+    <a href="javascript:void(0);" id="item_6">Contact</a>
+    </li>
+</ul>
